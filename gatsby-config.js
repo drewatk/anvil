220dad495aaca4577be5dd836abd972463dfa8d9
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-22 16:48:09 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: "The Anvil",
+    description: "Description"
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+      }
+    },
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["src"]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ]
+};
---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+import Typography from "typography";
+import fairyGatesTheme from "typography-theme-fairy-gates";
+
+fairyGatesTheme.overrideThemeStyles = () => ({
+  a: {
+    textShadow: "none"
+  }
+});
+
+const typography = new Typography(fairyGatesTheme);
+
+export default typography;
