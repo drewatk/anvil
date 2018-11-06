7754c87c7164b9f7b29fe07475bfc12694562f19
Drew Atkinson <drewatkinson5@gmail.com>
2018-11-06 16:02:50 -0500

    Updates

---
+++ component---src-pages-page-2-js-c18e1943816718d65df5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{138:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(142),l=t(143);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-c18e1943816718d65df5.js.map
