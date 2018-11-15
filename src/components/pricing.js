f5d8665a15b040f805d69f551a8aa0dcc2a7c6d9
Vritant Bhardwaj <vritoku@gmail.com>
2018-11-15 13:44:14 -0500

    looks pretty good

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -11,13 +11,19 @@ const Pricing = () => (
     <div className={styles.leftTriangle} />
     <div className={styles.leftContent}>
       <h4>Students</h4>
+      <h3><bold>$10</bold></h3>
+      <h5>Per Month</h5>
+      <h4><a className={styles.joinLink} href="">Join Now</a></h4>
     </div>
     <div className={styles.rhombus}>
       <h4>Join Now</h4>
     </div>
     <div className={styles.rightTriangle} />
     <div className={styles.rightContent}>
-      <h4>Community Members</h4>
+      <h4><a className={styles.joinLink} href="">Join Now</a></h4>
+      <h3><bold>$10</bold></h3>
+      <h5>Per Month</h5>
+      <h4>Non-Students</h4>
     </div>
   </div>
 );
