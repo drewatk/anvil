bd1427a01a05fd4836110bee2f6839208391eef2
Drew Atkinson <drewatkinson5@gmail.com>
2018-12-03 10:37:15 -0500

    Squashed commit of the following:
    commit e18bf54296bd9b6b0252428ee65ef5eea236af9c
    Author: Drew Atkinson <drewatkinson5@gmail.com>
    Date:   Mon Dec 3 10:08:52 2018 -0500
    
        Add GA, sponsor page

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -8,28 +8,69 @@ import TextSection from "../components/text-section";
 import PatternGreen from "../assets/pattern_green.svg";
 
 const EventsPage = ({ data, location }) => {
-  const section1 = {
-    title: "The Boiler",
-    subheading: "Lorem ispum blah blah blah",
-    paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus auctor vestibulum. Ut efficitur scelerisque sagittis. Aliquam dignissim fringilla laoreet. Sed auctor erat id erat fermentum eleifend. Nam arcu magna, egestas nec sem a, ultrices fringilla augue. Praesent ac feugiat ex, at vulputate diam. Maecenas vel hendrerit lorem, ac faucibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis dolor imperdiet, blandit ex in, congue risus."
-    ],
-    imageFluid: data.imageOne.childImageSharp.fluid
-  };
-  const section2 = {
-    title: "Section 2",
-    subheading: "Lorem ispum blah blah blah",
-    paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus auctor vestibulum. Ut efficitur scelerisque sagittis. Aliquam dignissim fringilla laoreet. Sed auctor erat id erat fermentum eleifend. Nam arcu magna, egestas nec sem a, ultrices fringilla augue. Praesent ac feugiat ex, at vulputate diam. Maecenas vel hendrerit lorem, ac faucibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis dolor imperdiet, blandit ex in, congue risus."
-    ]
-  };
+  const sections = [
+    {
+      title: "Sponsor The Boiler",
+      subheading: "Purdue's Student-run Startup Accelerator",
+      paragraphs: [
+        <>
+          We are always looking for support for our startup accelerator program,
+          The Boiler. If you are interested in sponsoring you can look at our
+          packet below. We are open to negotiation so feel free to contact us if
+          you have any questions
+        </>,
+        <>
+          For more information, refer to{" "}
+          <a
+            href={data.boilerPacket.publicURL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The Boiler Sponsorship Packet
+          </a>
+          .
+        </>,
+        <>
+          Feel free to contact us any time at{" "}
+          <a href="mailto:contact@anvilstartups.com">
+            contact@anvilstartups.com
+          </a>
+          .
+        </>
+      ],
+      imageFluid: data.boilerImage.childImageSharp.fluid,
+      alt: "Cryptoleague pitching at Demo Day 2018"
+    },
+    {
+      title: "Sponsor The Anvil",
+      paragraphs: [
+        <>
+          We're also always looking for support to keep building our community
+          through events, outreach, and our coworking space. If you'd like to
+          Sponsor The Anvil, you can refer to our{" "}
+          <a
+            href={data.anvilPacket.publicURL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Anvil Sponsorship Packet
+          </a>
+          .
+        </>
+      ]
+    }
+  ];
 
-  const textSections = [section1, section2];
   return (
     <Layout location={location}>
       <TitleSection Pattern={PatternGreen} title="Sponsor" theme="green" />
-      {textSections.map((section, index) => (
-        <TextSection Pattern={PatternGreen} {...section} index={index} />
+      {sections.map((section, index) => (
+        <TextSection
+          Pattern={PatternGreen}
+          {...section}
+          index={index}
+          key={index}
+        />
       ))}
     </Layout>
   );
@@ -39,7 +80,13 @@ export default EventsPage;
 
 export const pageQuery = graphql`
   query {
-    imageOne: file(relativePath: { eq: "img/boiler.jpg" }) {
+    boilerPacket: file(relativePath: { eq: "pdf/Boiler Sponsorship.pdf" }) {
+      publicURL
+    }
+    anvilPacket: file(relativePath: { eq: "pdf/Sponsorship Packet 2018.pdf" }) {
+      publicURL
+    }
+    boilerImage: file(relativePath: { eq: "img/boiler2.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
